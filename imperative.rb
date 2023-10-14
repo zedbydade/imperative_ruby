ae503995233a329e449a53bc971ac051e41a30e7
Francisco Leite <franciscoleite.dev@protonmail.com>
2023-10-14 00:58:05 -0300

    Add return state

--- imperative.rb
+++ imperative.rb
@@ -4,8 +4,18 @@ class ImperativeBuilder
   def initialize(expression)
     @expression = expression
   end
+
+  def return(value)
+    value
+  end
+
+  def zero(_value); end
+
+  def delay(&block)
+    block.call
+  end
 end
 
-def test_puts 
-  ImperativeBuilder.new("return 0 printfn 'after return' return 1")
+def test_puts
+  ImperativeBuilder.new("return 0 po 'after return' return 1")
 end
